ef6db7782173ebb1efce6d617ebfd2098918b8a1
Juringo83 <98878140+Juringo83@users.noreply.github.com>
2022-04-21 14:52:16 +0200

    project added to git

---
+++ src/test/java/de/hfu/UpperCaseTest.java
@@ -0,0 +1,13 @@
+package de.hfu;
+import java.util.Scanner;
+
+public class UpperCaseTest {
+	public static void main(String[] args) {
+		try(Scanner scanner = new Scanner(System.in)){
+		    System.out.println("Bitte geben Sie einen Text in Kleinbuchstaben ein:");
+		    String text = scanner.nextLine();
+		    text = text.toUpperCase();
+		    System.out.println(text);
+		}
+	}
+}
